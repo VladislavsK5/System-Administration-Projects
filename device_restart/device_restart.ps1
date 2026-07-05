45d11c787c5e93c5100268dde87a8408fb6c0dc7
VladislavsK5 <kurss.vladislavs@gmail.com>
2026-07-05 11:50:26 +0300

    Add files via upload

---
+++ device_restart/device_restart.ps1
@@ -0,0 +1,13 @@
+﻿#Device name
+$adapterName = "" #HERE
+
+while ($true) {
+    Write-Host "$(Get-Date) — restarting '$adapterName'..."
+    
+    Disable-NetAdapter -Name $adapterName -Confirm:$false
+    Start-Sleep -Seconds 5
+    Enable-NetAdapter -Name $adapterName -Confirm:$false
+    
+    Write-Host "Done."
+    Start-Sleep -Seconds 600
+}
